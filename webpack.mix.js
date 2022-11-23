a0396067e52fd79bfd7062267f3e920024e78798
meemnaeem <meemnaeem@gmail.com>
2022-11-22 20:05:46 -0800

    sass installed and working

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,16 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss"); /* Add this line at the top */
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss')
-    ])
-    .disableNotifications()
-    .webpackConfig(require('./webpack.config'));
+mix
+  .js("resources/js/app.js", "public/js")
+  .vue()
+  .sass("resources/sass/app.scss", "public/css")
+  .options({
+    postCss: [tailwindcss("./tailwind.config.js")],
+  })
+  .disableNotifications()
+  .webpackConfig(require("./webpack.config"));
 
 if (mix.inProduction()) {
-    mix.version();
+  mix.version();
 }
